c7064ebd83b6734114597622eec6c95400a0b85c
khanras <khanras@Khanraslt01.ap.teo.earth>
2019-01-23 18:11:46 +0530

    update

--- src/testCases/TC03.java
+++ src/testCases/TC03.java
@@ -1,12 +1,12 @@
 package testCases;
 
 import org.testng.annotations.Test;
-import actions.LoginPage;
+import actions.MobileAppPage;
 
 public class TC03 extends SuitClass {
 	
 	@Test
 	public void checkMobileAppNavigation() throws Exception {
-		new LoginPage().login(testExector);
+		new MobileAppPage().mobileAppNavigation(super.testExector);
 	}
 }
